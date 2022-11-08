290522aaa3a18267e63effea105221b5f18a8400
Tina <78636627+SUcy6@users.noreply.github.com>
2022-11-09 01:43:42 +0800

    upload

--- painter/hand.py
+++ painter/hand.py
@@ -7,6 +7,25 @@ import mediapipe as mp
 ############################################################################
 ############################################################################
 # useful functions
+def fingersUp(lmList):
+    fingers = []
+    # Thumb
+    if lmList[tipIds[0]][1] > lmList[tipIds[0] - 1][1]:
+        fingers.append(1)
+    else:
+        fingers.append(0)
+ 
+    # Fingers
+    for id in range(1, 5):
+        if lmList[tipIds[id]][2] < lmList[tipIds[id] - 2][2]:
+            fingers.append(1)
+        else:
+            fingers.append(0)
+ 
+        # totalFingers = fingers.count(1)
+ 
+    return fingers
+
 
 
 
@@ -16,18 +35,31 @@ import mediapipe as mp
 font = cv2.FONT_HERSHEY_SIMPLEX
 
 # Colors.
-blue = (255, 127, 0)
-red = (50, 50, 255)
+blue = (250, 206, 135)
+red = (49, 49, 255)
 green = (127, 255, 0)
 dark_blue = (127, 20, 0)
 light_green = (127, 233, 100)
-yellow = (0, 255, 255)
+yellow = (0, 234, 255)
 pink = (255, 0, 255)
+purple = (128, 0, 128)
+orange = (80, 127, 250)
+
+drawColor = (0, 0, 0)
+
+# thickness
+brushThickness = 25
+eraserThickness = 100
+
+# thumb, index, middle finger, ring finger, pinky
+tipIds = [4, 8, 12, 16, 20]
+xp, yp = 0, 0
+
+# get header
+header = cv2.imread("Header/header.png")
 
 # Initialize mediapipe class.
-# pose
-mp_pose = mp.solutions.pose
-pose = mp_pose.Pose()
+
 # hands
 mpHands = mp.solutions.hands
 hands = mpHands.Hands(static_image_mode=True,max_num_hands=2,min_detection_confidence=0.5)
@@ -43,13 +75,17 @@ if __name__ == "__main__":
     
     # Meta data
     fps = int(cap.get(cv2.CAP_PROP_FPS))
-    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    # width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    # height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    width = 1280
+    height = 720
     frame_size = (width, height)
     fourcc = cv2.VideoWriter_fourcc(*'mp4v')
 
     # Video writer.
     video_output = cv2.VideoWriter('output.mp4', fourcc, fps, frame_size)
+
+    imgCanvas = np.zeros((720, 1280, 3), np.uint8)
 
     while cap.isOpened():
         # Capture frames.
@@ -69,16 +105,14 @@ if __name__ == "__main__":
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
         # Process the image.
-        keypoints = pose.process(image)
         results = hands.process(image)
 
         # Convert the image back to BGR.
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
-        # Use wlm and lmPose as representative of the following methods.
-        wlm = keypoints.pose_world_landmarks
-        lm = keypoints.pose_landmarks
-        lmPose = mp_pose.PoseLandmark
+        # hands
+        # hl = results.multi_hand_landmarks[0]
+        # hr = results.multi_hand_landmarks[1]
 
         xList = []
         yList = []
@@ -86,98 +120,101 @@ if __name__ == "__main__":
         lmList = []
         if results.multi_hand_landmarks:
                 myHand = results.multi_hand_landmarks[0]
-                print(myHand)
+
+                # draw hand landmarks
+                for handLms in results.multi_hand_landmarks:
+                    mpDraw.draw_landmarks(image, handLms, mpHands.HAND_CONNECTIONS)
+
                 for id, lm in enumerate(myHand.landmark):
                     # print(id, lm)
-                #     # get x and y for each node on hand
+                    # get x and y for each node on hand
                     cx, cy = int(lm.x * w), int(lm.y * h)
-                #     xList.append(cx)
-                #     yList.append(cy)
-                #     # print(id, cx, cy)
-                #     # append coordinate into list
-                #     lmList.append([id, cx, cy])
+                    xList.append(cx)
+                    yList.append(cy)
+                    # print(id, cx, cy)
+                    # append coordinate into list
+                    lmList.append([id, cx, cy])
 
-                # # get bounding box
-                # xmin, xmax = min(xList), max(xList)
-                # ymin, ymax = min(yList), max(yList)
-                # bbox = xmin, ymin, xmax, ymax
+                # get bounding box
+                xmin, xmax = min(xList), max(xList)
+                ymin, ymax = min(yList), max(yList)
+                bbox = xmin, ymin, xmax, ymax
+
+        if len(lmList) != 0:
+            # print(lmList[4])
+
+            # tip of index and middle fingers
+            x1, y1 = lmList[8][1:]
+            x2, y2 = lmList[12][1:]
+
+            # Check which fingers are up
+            fingers = fingersUp(lmList)
+
+            # Selection Mode - Two finger are up
+            if fingers[1] and fingers[2]:
+                xp, yp = 0, 0
+                # print("Selection Mode")
+                # Checking for the click
+                if y1 < 125:
+                    if 160 < x1 < 300: 
+                        drawColor = purple
+                    elif 325 < x1 < 470:
+                        drawColor = orange
+                    elif 500 < x1 < 640:
+                        drawColor = yellow
+                    elif 675 < x1 < 800:
+                        drawColor = blue
+                    elif 840 < x1 < 960:
+                        drawColor = red
+                    elif 990 < x1 < 1120:
+                        drawColor = (0,0,0)
+                cv2.rectangle(image, (x1, y1 - 25), (x2, y2 + 25), drawColor, cv2.FILLED)
+
+            # Drawing Mode - Index finger is up
+            if fingers[1] and fingers[2] == False:
+                cv2.circle(image, (x1, y1), 15, drawColor, cv2.FILLED)
+                # print("Drawing Mode")
+                if xp == 0 and yp == 0:
+                    xp, yp = x1, y1
  
- 
-
-
-        try: 
-            # Acquire the world landmark coordinates.
-            mpt_shoulder_wx = (wlm.landmark[lmPose.LEFT_SHOULDER].x * 1000  + wlm.landmark[lmPose.RIGHT_SHOULDER].x * 1000 )/2
-            mpt_shoulder_wy = (wlm.landmark[lmPose.LEFT_SHOULDER].y * 1000  + wlm.landmark[lmPose.RIGHT_SHOULDER].y * 1000 )/2
-            mpt_shoulder_wz = (wlm.landmark[lmPose.LEFT_SHOULDER].z * 1000  + wlm.landmark[lmPose.RIGHT_SHOULDER].z * 1000 )/2
-            #mpt_shoulder_z = (lm.landmark[lmPose.LEFT_SHOULDER].z * c + lm.landmark[lmPose.RIGHT_SHOULDER].z * c)/2
-
-            mpt_hip_wx = (wlm.landmark[lmPose.LEFT_HIP].x * 1000  + wlm.landmark[lmPose.RIGHT_HIP].x * 1000 )/2
-            mpt_hip_wy = (wlm.landmark[lmPose.LEFT_HIP].y * 1000  + wlm.landmark[lmPose.RIGHT_HIP].y * 1000 )/2
-            mpt_hip_wz = (wlm.landmark[lmPose.LEFT_HIP].z * 1000  + wlm.landmark[lmPose.RIGHT_HIP].z * 1000 )/2
-            mpt_hip_extend_wx = mpt_hip_wx 
-            mpt_hip_extend_wy = 0
-            mpt_hip_extend_wz = mpt_hip_wz
+                cv2.line(image, (xp, yp), (x1, y1), drawColor, brushThickness)
+                if drawColor == (0, 0, 0):
+                    cv2.line(image, (xp, yp), (x1, y1), drawColor, eraserThickness)
+                    cv2.line(imgCanvas, (xp, yp), (x1, y1), drawColor, eraserThickness)
             
-            left_shoulder_wx = int(wlm.landmark[lmPose.LEFT_SHOULDER].x * 1000)
-            left_shoulder_wy = int(wlm.landmark[lmPose.LEFT_SHOULDER].y * 1000)
-            left_shoulder_wz = int(wlm.landmark[lmPose.LEFT_SHOULDER].z * 1000)
+                else:
+                    cv2.line(image, (xp, yp), (x1, y1), drawColor, brushThickness)
+                    cv2.line(imgCanvas, (xp, yp), (x1, y1), drawColor, brushThickness)
+
+                xp, yp = x1, y1
+
+            if fingers[1] == False:
+                xp, yp = 0, 0
+
+
+            # Clear Canvas when all fingers are up
+            if all (x >= 1 for x in fingers):
+                imgCanvas = np.zeros((720, 1280, 3), np.uint8)
             
-            right_shoulder_wx = int(wlm.landmark[lmPose.RIGHT_SHOULDER].x * 1000)
-            right_shoulder_wy = int(wlm.landmark[lmPose.RIGHT_SHOULDER].y * 1000)
-            right_shoulder_wz = int(wlm.landmark[lmPose.RIGHT_SHOULDER].z * 1000)
-            
-            left_hip_wx = int(wlm.landmark[lmPose.LEFT_HIP].x * 1000)
-            left_hip_wy = int(wlm.landmark[lmPose.LEFT_HIP].y * 1000)
-            left_hip_wz = int(wlm.landmark[lmPose.LEFT_HIP].z * 1000)
-            
-            right_hip_wx = int(wlm.landmark[lmPose.RIGHT_HIP].x * 1000)
-            right_hip_wy = int(wlm.landmark[lmPose.RIGHT_HIP].y * 1000)
-            right_hip_wz = int(wlm.landmark[lmPose.RIGHT_HIP].z * 1000)
-            
-            
-            # Acquire the landmark coordinates.
-            mpt_shoulder_x = int((lm.landmark[lmPose.LEFT_SHOULDER].x * w  + lm.landmark[lmPose.RIGHT_SHOULDER].x * w )/2)
-            mpt_shoulder_y = int((lm.landmark[lmPose.LEFT_SHOULDER].y * h  + lm.landmark[lmPose.RIGHT_SHOULDER].y * h )/2)
-            mpt_shoulder_z = int((lm.landmark[lmPose.LEFT_SHOULDER].z * w  + lm.landmark[lmPose.RIGHT_SHOULDER].z * w )/2)
-            #mpt_shoulder_z = (lm.landmark[lmPose.LEFT_SHOULDER].z * c + lm.landmark[lmPose.RIGHT_SHOULDER].z * c)/2
-
-            mpt_hip_x = int((lm.landmark[lmPose.LEFT_HIP].x * w  + lm.landmark[lmPose.RIGHT_HIP].x * w )/2)
-            mpt_hip_y = int((lm.landmark[lmPose.LEFT_HIP].y * h  + lm.landmark[lmPose.RIGHT_HIP].y * h )/2)
-            mpt_hip_z = int((lm.landmark[lmPose.LEFT_HIP].z * w  + lm.landmark[lmPose.RIGHT_HIP].z * w )/2)
-            mpt_hip_extend_x = mpt_hip_x 
-            mpt_hip_extend_y = 0
-            mpt_hip_extend_z = mpt_hip_z
-
-
-        except AttributeError:
-            continue 
-        # Draw
-        # imageX1 = int(mpt_shoulder_x)
-        # imageX2 = int(mpt_hip_x)
-        # imageX3 = int(mpt_hip_extend_x)
-        # imageY1 = int(mpt_shoulder_y)
-        # imageY2 = int(mpt_hip_y)
-        # imageY3 = int(mpt_hip_extend_y)
-
-        #draw circle
-        # cv2.circle(image, (imageX1, imageY1), 7, yellow, -1)
-        # cv2.circle(image, (imageX2, imageY2), 7, yellow, -1)
-        # cv2.circle(image, (imageX3, imageY3), 7, yellow, -1)
-
-        # #draw line
-        # cv2.line(image, (imageX1, imageY1), (imageX2, imageY2), (255, 255, 255), 3)
-        # cv2.line(image, (imageX2, imageY2), (imageX3, imageY3), (255, 255, 255), 3)
-
-
-        # draw hands
-        if results.multi_hand_landmarks:
-            for handLms in results.multi_hand_landmarks:
-                mpDraw.draw_landmarks(image, handLms, mpHands.HAND_CONNECTIONS)
+        
+        imgGray = cv2.cvtColor(imgCanvas, cv2.COLOR_BGR2GRAY)
+        _, imgInv = cv2.threshold(imgGray, 50, 255, cv2.THRESH_BINARY_INV)
+        imgInv = cv2.cvtColor(imgInv,cv2.COLOR_GRAY2BGR)
+        image = cv2.bitwise_and(image,imgInv)
+        image = cv2.bitwise_or(image,imgCanvas)
       
         # Display.
         image = cv2.flip(image, 1)
+        # imgCanvas = cv2.flip(imgCanvas, 1)
+        # imgInv = cv2.flip(imgInv, 1)
+        # image = cv2.addWeighted(image,0.5,imgCanvas,0.5,0)
+        image[0:125, 0:1280] = header
         cv2.imshow('MediaPipe Pose', image)
+        # cv2.imshow("Canvas", imgCanvas)
+        # cv2.imshow("Inv", imgInv)
+        cv2.waitKey(1)
+
+        # press 'q' to quit the game
         if cv2.waitKey(5) & 0xFF == ord('q'):
             break
 
